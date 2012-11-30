f4a655b6726ebe847f5c23b946326e8fa8e0f905
tobruens <tobruens@htwg-konstanz.de>
2012-11-30 12:27:26 +0100

    Änderung an Test

--- PacManProjekt/test/pacman/GridValuesTest.java
+++ PacManProjekt/test/pacman/GridValuesTest.java
@@ -1,46 +1,79 @@
 package pacman;
 
 import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class GridValuesTest {
-	
-	private boolean[] ghost;
-	private boolean[] player;
-	private boolean[] isWall;
-	private boolean[] food;
-	
+public class GridValuesTest extends TestCase {
+
 	GridValues gv;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		ghost[0] = false;
-		player[0] = false;
-		isWall[0] = false;
-		food[0] = false;
-		
 		gv = new GridValues();
-		gv.create(1);
-		gv.setFood(0, false);
-		gv.setGhost(0, false);
-		gv.setPlayer(0, false);
-		gv.setWall(0, false);
+		assertNotNull(gv);
+		gv.create(2);	
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		ghost = null;
-		player = null;
-		isWall = null;
-		food = null;
+		gv = null;
 	}
-	
-	public void testfieldIsEmpty() {
+
+	@Test
+	public final void testFieldIsEmpty() {
+		gv.setGhost(0, false);
+		assertFalse(gv.isGhost(0));
+		
+		gv.setFood(0, false);
+		assertFalse(gv.isFood(0));
+	}
+
+	@Test
+	public final void testIsGhost() {
+		gv.setGhost(0, false);
 		assertFalse(gv.isGhost(0));
 	}
 
+	@Test
+	public final void testIsPlayer() {
+		gv.setPlayer(0, false);
+		assertFalse(gv.isPlayer(0));
+	}
+/*
+	@Test
+	public final void testIsWall() {
+		fail("Not yet implemented"); // TODO
+	}
 
+	@Test
+	public final void testIsFood() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetGhost() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetPlayer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetWall() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetFood() {
+		fail("Not yet implemented"); // TODO
+	}
+*/
 }
